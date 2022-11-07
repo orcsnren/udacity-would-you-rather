deb4ce7261638b5699006925b575e921c242c241
Soner <sonereren39@gmail.com>
2022-11-08 00:55:02 +0300

    Feature store and reducers are generated.

---
+++ src/reducers/questions.js
@@ -0,0 +1,11 @@
+export default function questions(state = {}, action) {
+    switch (action.type) {
+        case 'RECEIVE_QUESTIONS':
+            return {
+                ...state,
+                ...action.questions
+            };
+        default:
+            return state
+    }
+}
---
+++ src/reducers/users.js
@@ -0,0 +1,8 @@
+export default function users(state = {}, action) {
+    switch (action.type) {
+        case 'RECEIVE_USERS':
+            return { ...state, ...action.users };
+        default:
+            return state;
+    }
+}
