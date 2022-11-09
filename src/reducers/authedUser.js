cdd7487ce3733161145e5ffc627783560b8c9ac0
Soner <sonereren39@gmail.com>
2022-11-10 00:46:16 +0300

    Feature reducers update

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,8 +1,10 @@
-export default function authedUser(state = null, action) {
-    switch (action.type) {
-        case 'SET_AUTHED_USER':
-            return action.id;
-        default:
-            return state;
-    }
+import { SET_AUTHED_USER } from '../actions/authedUser'
+
+export default function authedUser (state = null, action) {
+  switch (action.type) {
+    case SET_AUTHED_USER :
+      return action.id
+    default :
+    return state
+  }
 }
